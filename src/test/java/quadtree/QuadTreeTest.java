04ad526afded3a84643a43df3b3f9e478805359c
hyunho <oksknoh@gmail.com>
2012-08-03 17:49:22 +0900

    QuadTree mapper is removed

--- src/test/java/quadtree/QuadTreeTest.java
+++ src/test/java/quadtree/QuadTreeTest.java
@@ -8,30 +8,10 @@ import org.apache.hadoop.mapred.OutputCollector;
 import org.junit.*;
 
 import quadtree.io.*;
-import quadtree.QuadTreeMapper.*;
 
 public class QuadTreeTest {
 
-	@Test
-	public void testMap() throws IOException {
-		QuadTreeMapper mapper = new QuadTreeMapper();
 
-		// temperature : -11
-		// atmosphericPressure : 10268
-		Text record = new Text("004301199099999"+"1950"+"051518004+68750+023550FM-12+0382" +
-				"99999V0203201N00261220001CN9999999N9"+"-00111"+"+99999"+"102681");
-
-		//mean of temperature = (max(618) + min(-932)) / 2 = -243 
-		//mean of atmosphericPressure = (max(10900) + min(8600) / 2 = 9750		
-		OutputCollector<Text, TupleWritableComparable> output = mock(OutputCollector.class);
-
-		mapper.setQuadtreeDepth(1);		
-		mapper.map(null, record, output, null);
-
-		verify(output).collect(new Text("11"), new TupleWritableComparable(-11, 10268));
-		verify(output, never()).collect(new Text("11"), new TupleWritableComparable(-10, 10268));
-	}
-	
 	@Test
 	public void testParsingMap() throws IOException {		
 		ParsingMapper parsingMapper = new ParsingMapper();

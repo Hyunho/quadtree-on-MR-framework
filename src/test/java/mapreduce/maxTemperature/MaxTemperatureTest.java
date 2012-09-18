251b1aef3bca2988145388056f8a1fdfeea0be26
hyunho <oksknoh@gmail.com>
2012-09-18 17:01:11 +0900

    merge files

--- src/test/java/mapreduce/maxTemperature/MaxTemperatureTest.java
+++ src/test/java/mapreduce/maxTemperature/MaxTemperatureTest.java
@@ -1,7 +1,10 @@
 package mapreduce.maxTemperature;
 import static org.mockito.Matchers.anyObject;
 import static org.mockito.Mockito.*;
+
 import java.io.IOException;
+import java.util.Arrays;
+import java.util.Iterator;
 
 import mapreduce.maxTemperature.MaxTemperatureMapper;
 
@@ -10,7 +13,7 @@ import org.apache.hadoop.mapred.OutputCollector;
 
 import org.junit.*;
 
-public class MaxTemperatureMapperTest {
+public class MaxTemperatureTest {
 	
 	@Test
 	public void processesValidRecord() throws IOException {
@@ -39,5 +42,21 @@ public class MaxTemperatureMapperTest {
 
 		verify(output, never()).collect(any(Text.class), any(IntWritable.class));
 	}
+	
+	@Test
+	public void returnsMaximumIntegerInValues() throws IOException {
+		
+		MaxTemperatureReducer reducer = new MaxTemperatureReducer();
+		
+		Text key = new Text("1950");		
+		Iterator<IntWritable> values = Arrays.asList(
+				new IntWritable(10), new IntWritable(5)).iterator();
+		
+		OutputCollector<Text, IntWritable> output = mock(OutputCollector.class);
+		
+		reducer.reduce(key, values, output, null);		
+		verify(output).collect(key, new IntWritable(10));
+	}
+	
 	
 }

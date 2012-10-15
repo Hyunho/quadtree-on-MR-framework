66ed631e4966f4c04cf4c5a7a344714ec250f8f1
hyunho <oksknoh@gmail.com>
2012-10-15 12:06:08 +0900

    modify r-script to pass aguments on command line

--- src/test/RScript/gen_data.R
+++ src/test/RScript/gen_data.R
@@ -1,3 +1,7 @@
-count = 10000
+#!/usr/bin/Rscript  
+
+argv <- commandArgs(TRUE);
+output = argv[1]
+count = 100
 df = data.frame(x= runif(count, min=0, max=100), y= runif(count, min =0, max = 100))
-write.table(df, file="data2D-quad.txt", row.names = FALSE, col.names=FALSE)
+write.table(df, file=output, row.names = FALSE, col.names=FALSE)

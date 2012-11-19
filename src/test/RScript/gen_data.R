6fe4949678df420b2755fd16d47d944e6f56baba
hyunho <oksknoh@gmail.com>
2012-11-20 01:23:04 +0900

    edit script to generage multi-dimension and change range per each dimension from [0, 100] to [0, 1000].

--- src/test/RScript/gen_data.R
+++ src/test/RScript/gen_data.R
@@ -1,7 +1,23 @@
 #!/usr/bin/Rscript  
 
+
 argv <- commandArgs(TRUE);
+
+#first argument is filename to save data.
+#second arggument is number of dimension. 
 output = argv[1]
+numDim = as.numeric(argv[2])
 count = 100
-df = data.frame(x= runif(count, min=0, max=100), y= runif(count, min =0, max = 100))
-write.table(df, file=output, row.names = FALSE, col.names=FALSE)
+
+
+origin_frame = data.frame(runif(count, min=0, max=1000))
+numDim = numDim - 1
+
+while(numDim > 0) {
+  ##make new frame and bind origin frame and new one
+  new_frame = data.frame(runif(count, min=0, max=1000))
+  origin_frame = cbind(origin_frame, new_frame)
+  numDim = numDim - 1
+}
+
+write.table(origin_frame, file=output, row.names = FALSE, col.names=FALSE)

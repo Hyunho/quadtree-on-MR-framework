0dee4f3c6562e4d8e7cd01f43ef0b84a83252401
hyunho <oksknoh@gmail.com>
2012-11-28 12:05:26 +0900

    support range query

--- src/test/java/index/quadtree/HelpQuadtreeTest.java
+++ src/test/java/index/quadtree/HelpQuadtreeTest.java
@@ -1,9 +1,13 @@
 package index.quadtree;
 
+import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.CoreMatchers.not;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertThat;
 
+import index.quadtree.QuadtreeSearcher.NormalSearcher;
+
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Iterator;
 import java.util.List;
@@ -39,6 +43,34 @@ public class HelpQuadtreeTest {
 		}		
 		
 		return quadtree;
+	}
+	
+	@Test
+	public void rangeQuery() {
+		
+		Boundary boundary = new Boundary(new Range(0, 11), new Range(0, 11));
+
+		QuadTreeFile quadtree = HelpQuadtreeTest.build();
+		
+		NormalSearcher searcher = new QuadtreeSearcher.NormalSearcher(quadtree);
+		
+		ArrayList<Point> answers = new ArrayList<Point>();
+				
+		ArrayList<QuadTreeFile> nodes = searcher.searchNodes(boundary);
+		
+		assertThat(0, not(nodes.size()));
+		for(QuadTreeFile node : nodes) {
+			Iterator<Point> points = node.points();
+			while(points.hasNext()) {
+				Point point = points.next();
+				if(boundary.containsPoint(point)) {
+					answers.add(point);
+				}
+					
+			}
+		}
+
+		assertThat(answers.size(), is(2));
 	}
 }
 
